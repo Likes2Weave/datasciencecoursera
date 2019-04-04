79950cb6ba99e64f1e9973537ce4d48f4b5387c1
Coleen Smith <smithdcrk@gmail.com>
2019-04-04 10:35:58 -0500

    Incremental Update
    Changes so far to R script and Readme.rmd

--- Course3/run_analysis.R
+++ Course3/run_analysis.R
@@ -7,3 +7,18 @@
 ## 5. From the data set in step 4, creates a second, independent tidy data set
 ##    with the average of each variable for each activity and each subject.
 
+## Data should be tidy:
+## 1. Each variable forms a column.
+## 2. Each observation forms a row.
+## 3. Each type of observational unit forms a table.
+
+install.packages("R.utils")
+library("R.utils")
+
+## Read test data into data files
+## Name of the data file will match name of text file
+
+subject_test <- read.table("UCI_HAR_Dataset/test/subject_test.txt", header = FALSE)
+X_test <- read.table("UCI_HAR_Dataset/test/X_test.txt", header = FALSE)
+Y_test <- read.table("UCI_HAR_Dataset/test/Y_test.txt", header = FALSE)
+
